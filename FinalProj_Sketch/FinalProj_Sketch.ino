141d0225a041f121022d9d636d0410616b531386
Thomas <tbraun@nevada.unr.edu>
2023-05-08 21:35:02 -0700

    Loop function layout

--- FinalProj_Sketch/FinalProj_Sketch.ino
+++ FinalProj_Sketch/FinalProj_Sketch.ino
@@ -1,5 +1,20 @@
 //Thomas Braun, Richard White, Tristan Braum
+//May 9, 2023
+//CPE 301
 //Final Project: Swamp Cooler
+
+//i think the port for the motor is wrong
+//i dont have the ports for the buttons setup yet either
+//we should defo check all the ports to make sure they are correct
+//bool test added until i can figure out how to press a button
+
+//do we need to print to serial monitor???
+//print error message to serial monitor
+    /*char print[20] = "Water level is low\n";
+    for(int i = 0; i < 20; i++){
+      U0putchar(print[i]);
+      U0getchar();
+    }*/
 
 #include <LiquidCrystal.h>
 #include <Stepper.h>
@@ -10,11 +25,10 @@
 #define RDA 0x80
 #define TBE 0x20  
 
-//blue led 10
-//green 9
-//yellow 8
-//red 7
-//A1 water level sensor
+//A1 water level sensor , PF1
+//STOP button: pwm 2 , PE0
+//RESET button: digital 52 , PB1
+//Potentiometer: Analog 0, PF0
 
 volatile unsigned char *myUCSR0A = (unsigned char*)0x00C0;
 volatile unsigned char *myUCSR0B = (unsigned char*)0x00C1;
@@ -26,32 +40,36 @@ volatile unsigned char *my_ADCSRB   = (unsigned char*) 0x7B;
 volatile unsigned char *my_ADCSRA   = (unsigned char*) 0x7A;
 volatile unsigned int  *my_ADC_DATA = (unsigned int*)  0x78;
 
-volatile unsigned char *portDDRB = (unsigned char*) 0x24;
 volatile unsigned char *portB    = (unsigned char*) 0x25;
+volatile unsigned char *portE    = (unsigned char*) 0x2E;
+volatile unsigned char *portF    = (unsigned char*) 0x31;
 volatile unsigned char *portH    = (unsigned char*) 0x102;
+volatile unsigned char *portDDRB = (unsigned char*) 0x24;
+volatile unsigned char *portDDRE = (unsigned char*) 0x2D;
+volatile unsigned char *portDDRF = (unsigned char*) 0x30;
 volatile unsigned char *portDDRH = (unsigned char*) 0x101;
 
-int state = 0;
+bool test = false;
 
-//Setup dht
+//DHT Setup
 const int DHT_PIN = 13;
 const int DHT_TYPE = DHT11;
 DHT dht(DHT_PIN, DHT_TYPE);
-float humidity = 0.0;
-float temperature = 0.0;
+//float humidity = 0.0;
+//float temperature = 0.0;
 
-//Setup stepper
+//Stepper motor setup
 const int STEPS = 4096;
 Stepper stepper(STEPS / 2, 23, 27, 25, 29); //num steps and pins connected to stepper 
 const int potPin = A0;
-int potVal = 0;
-int motorSpeed = 0;
+//int motorSpeed = 0;
 int currentPosition = 0;
 int previousPosition = 0;
 
-//Setup LCD
+//LCD Setup
 LiquidCrystal lcd(12, 11, 6, 5, 4, 3);
 const int backlightPin = 15; //pin number for the backlight pin
+
 
 void setup() {
 
@@ -72,90 +90,92 @@ void setup() {
   *portDDRB |= 0xF0;
   *portDDRH |= 0x60;
 
-  attachInterrupt(digitalPinToInterrupt(2), isr, RISING);
+  //set STOP button to interrupt
+  attachInterrupt(digitalPinToInterrupt(2), disable(), RISING);
+  //set REST button to interrupt for true / false
+  attachInterrupt(digitalPinToInterrupt(20) , reset() , RISING);
 }
 
 void loop() {
+  if (getWaterLevel < 120) {
+    changeLEDState(1); //error
+    stopFan();
 
-  if(state == 0){//is a state helpful if statement. It doesnt seem helpful and I dont know how to get out of the 0 state
-    disable();
+    //print error message to LCD
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Water Low!");
   }
-  else{
-
-    delay(500);//delay readings
-
-    //get values using sensor
-    temperature = dht.readTemperature();
-    humidity = dht.readHumidity();
-    unsigned int waterLevel = adc_read(5);
-    unsigned int potRead = adc_read(0);
-
-    checkWaterLevel(waterLevel);
-
-    potVal = adc_read(potPin);
-    motorSpeed = map(potVal, 0, 1023, 0, 100);
-    stepper.setSpeed(motorSpeed);
-    moveStepper();
-
-    lcd.clear();//reset screen
-    lcd.setCursor(0,0);//move curser back to starting position
-    lcd.print("Temp is ");
-    lcd.print(temperature);
-    lcd.setCursor(0,1);//set curser to the start of the second row
-    lcd.print("Humidity is");
-    lcd.print(humidity);
-
-    //turns on fan
-    fan(temperature);
-
+  else {
+    if (reset()) {
+      printTempHumidity();
+      moveStepper();
+      
+      if (getTemp() >= 24) {
+        changeLEDState(3); //running
+        startFan();
+      }
+      else {
+        changeLEDState(2); //idle
+        stopFan();
+      }
+    }
   }
 }
-void checkWaterLevel(unsigned int wl){
-  if(wl < 120){
-    char print[20] = "water level is low \n";
-    for(int i = 0; i < 20; i++){
-      U0putchar(print[i]);
-      U0getchar();
-    }
-    while(adc_read(5) < 120 && state != 0){
-      lcd.clear();
-      lcd.setCursor(0,0);
-      lcd.print("Water Low!");
 
-      //turn off fan
-      *portH &= 0xDF;//clear pin 13 PB5 to low
-      //turn red led
-      redLED(1);
-    }
-    //turn on fan
-    *portH |= 0x20; //set pin 37 PH5 to high
-    //Turn on green LED
-    greenLED(1);
-  }
-  delay(500);
+//returns temperature data
+float getTemp() {
+  return dht.readTemperature();
 }
+
+//returns humidity data
+float getHumidity() {
+  return dht.readHumidity();
+}
+
+//returns water level of resevoir
+unsigned int getWaterLevel() {
+  return adc_read(1);
+}
+
+//returns potentiometer reading
+unsigned int getPot() {
+  return adc_read(0);
+}
+
+//Start fan motor, set digital pin 37 to HIGH
+void startFan() {
+  *portH |= 0x20;
+}
+
+//Stop fan motor, clear digital pin 37 to LOW
+void stopFan() {
+  *portH &= 0xDF;
+}
+
+//Changes the state of the LED when requested from the loop function
+//sets all unused LED's to LOW
 void changeLEDState(int numLED){
-  //red is 1, green is 2, blue is 3, yellow is 4
   switch(numLED){
-    case 1:
+    case 1: //red
       redLED(1);
       greenLED(0);
       blueLED(0);
       yellowLED(0);
       break;
-    case 2:
+    case 2: //green
       redLED(0);
       greenLED(1);
       blueLED(0);
       yellowLED(0);
       break;
-    case 3:
+    case 3: //blue
       redLED(0);
       greenLED(0);
       blueLED(1);
       yellowLED(0);
       break;
-    case 4:
+    case 4: //yellow
       redLED(0);
       greenLED(0);
       blueLED(0);
@@ -166,77 +186,34 @@ void changeLEDState(int numLED){
   }
 }
 
-void isr() {
-  if(state != 0){
-    disable();
-    state = 0;
-  }
-  else{
-    state = 1;
-    *portB &= ~(1<<4);
-    *portH |= 1<<6;
-  }
+//print temperature and humidity data
+void printTempHumidity() {
+  lcd.clear();            
+  lcd.setCursor(0,0);     
+  lcd.print("Temp is ");
+  lcd.print(getTemp());
+  lcd.setCursor(0,1);     
+  lcd.print("Humidity is ");
+  lcd.print(getHumdity());
 }
 
-void fan(float temp) {
-  if (temp > 20) {
-    *portH |= 0x20; //set pin 37 PH5 to high
-    blueLED(1);
-  }else {
-    *portH &= 0xDF;//clear pin 13 PB5 to low
-    greenLED(1);
-  }
+//Disable motor fan and set LED to yellow
+void disable() {
+  changeLEDState(4); //yellow
+  stopFan();
+  *portB &= ~(1<<4); //turn off LCD? 
+  moveStepper();
 }
 
-void disable(){
-  *portH &= 0xDF;//clear pin 13 PB5 to low (Turns off fan)
-  yellowLED(1);//turn on yellowLED
-  lcd.clear();//Clears lcd
+void reset() {
+  test = true;
 }
 
-void U0init(unsigned long U0baud) {
- unsigned long FCPU = 16000000;
- unsigned int tbaud;
- tbaud = (FCPU / 16 / U0baud - 1);
- *myUCSR0A = 0x20;
- *myUCSR0B = 0x18;
- *myUCSR0C = 0x06;
- *myUBRR0  = tbaud;
-}
-void U0putchar(unsigned char U0pdata)
-{
-  while((*myUCSR0A & TBE)==0);
-  *myUDR0 = U0pdata;
-}
-
-void adc_init() {
-  *my_ADCSRA |= 0b10000000;
-  *my_ADCSRA &= 0b11011111; 
-  *my_ADCSRA &= 0b11110111; 
-  *my_ADCSRA &= 0b11111000; 
-  *my_ADCSRB &= 0b11110111; 
-  *my_ADCSRB &= 0b11111000; 
-  *my_ADMUX  &= 0b01111111; 
-  *my_ADMUX  |= 0b01000000; 
-  *my_ADMUX  &= 0b11011111; 
-  *my_ADMUX  &= 0b11100000; 
-}
-
-unsigned int adc_read(unsigned char adc_channel_num) {
-  *my_ADMUX  &= 0b11100000;
-  *my_ADCSRB &= 0b11110111;
-  if(adc_channel_num > 7) {
-    adc_channel_num -= 8;
-    *my_ADCSRB |= 0b00001000;
-  }
-  
-  *my_ADMUX  += adc_channel_num;
-  *my_ADCSRA |= 0x40;
-  while((*my_ADCSRA & 0x40) != 0);
-  return *my_ADC_DATA;
-}
-
+//moves stepper motor
 void moveStepper() {
+  //set speed
+  stepper.setSpeed(map(getPot(), 0, 1023, 0, 100));
+
   //calc num steps to move stepper
   int stepsToMove = abs(currentPosition - previousPosition);
   previousPosition = currentPosition;
@@ -258,6 +235,53 @@ void moveStepper() {
   }
 }
 
+//set comms rate
+void U0init(unsigned long U0baud) {
+ unsigned long FCPU = 16000000;
+ unsigned int tbaud;
+ tbaud = (FCPU / 16 / U0baud - 1);
+ *myUCSR0A = 0x20;
+ *myUCSR0B = 0x18;
+ *myUCSR0C = 0x06;
+ *myUBRR0  = tbaud;
+}
+
+//display output to serial monitor
+void U0putchar(unsigned char U0pdata) {
+  while((*myUCSR0A & TBE)==0);
+  *myUDR0 = U0pdata;
+}
+
+//Initialize ADC
+void adc_init() {
+  *my_ADCSRA |= 0b10000000;
+  *my_ADCSRA &= 0b11011111; 
+  *my_ADCSRA &= 0b11110111; 
+  *my_ADCSRA &= 0b11111000; 
+  *my_ADCSRB &= 0b11110111; 
+  *my_ADCSRB &= 0b11111000; 
+  *my_ADMUX  &= 0b01111111; 
+  *my_ADMUX  |= 0b01000000; 
+  *my_ADMUX  &= 0b11011111; 
+  *my_ADMUX  &= 0b11100000; 
+}
+
+//Read analog data
+unsigned int adc_read(unsigned char adc_channel_num) {
+  *my_ADMUX  &= 0b11100000;
+  *my_ADCSRB &= 0b11110111;
+  if(adc_channel_num > 7) {
+    adc_channel_num -= 8;
+    *my_ADCSRB |= 0b00001000;
+  }
+  
+  *my_ADMUX  += adc_channel_num;
+  *my_ADCSRA |= 0x40;
+  while((*my_ADCSRA & 0x40) != 0);
+  return *my_ADC_DATA;
+}
+
+//set LED to HIGH or LOW based on input
 void blueLED(bool on){
   if(on = 1){
     *portB |= 1<<2;//turns on pin 10
@@ -286,4 +310,3 @@ void redLED(bool on){
     *portB &= ~(1<<7);//turns off pin 7
   }
 }
-
